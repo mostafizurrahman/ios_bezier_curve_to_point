6e3d32750a640534a08f06f1a91eab8632f7e639
Mostafizur Rahman <mostafizur.cse@gmail.com>
2020-11-13 02:28:18 +0600

    bezier curve drawing with smooth UIBezierPath

--- PomaxBezier/PomaxBezier/ViewController.swift
+++ PomaxBezier/PomaxBezier/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var drawingView: DrawingView!
+    var touchPoints:[CGPoint] = []
     var drawingSize = CGSize.zero
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +24,11 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        self.counter = 0
         UIGraphicsBeginImageContext(drawingSize)
         for touch in touches {
-            self.draw(point: touch.preciseLocation(in: self.drawingView))
+            self.points[self.counter] = touch.preciseLocation(in: self.drawingView)  * drawingView.contentScaleFactor
+            break
         }
         
     }
@@ -33,35 +36,125 @@ class ViewController: UIViewController {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         for touch in touches {
-            self.draw(point: touch.preciseLocation(in: self.view))
+            self.draw(inPoint: touch.preciseLocation(in: self.view))
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         for touch in touches {
-            self.draw(point: touch.preciseLocation(in: self.view))
+            self.draw(inPoint: touch.preciseLocation(in: self.view))
         }
         
         UIGraphicsEndImageContext()
     }
     
-    func draw(point:CGPoint){
-        if let _context = UIGraphicsGetCurrentContext() {
-            _context.addEllipse(in: CGRect.init(origin: point * self.drawingView.contentScaleFactor,
-                                                size: CGSize(width: dotSize, height: dotSize)))
-//            _context.move(to: CGPoint(x: 0, y: <#T##CGFloat#>))
-            _context.drawPath(using: .fill)
-            _context.fillPath()
-//            _context.fillPath(using: CGPathFillRule.winding)
-            if let image = _context.makeImage() {
-                self.drawingView.image = image
+    var counter = 0
+    var points:[CGPoint] = Array.init(repeating: .zero, count: 5)
+    
+    func draw(inPoint:CGPoint){
+        self.counter += 1
+        self.points[self.counter] = inPoint * drawingView.contentScaleFactor
+        if self.counter == 4 {
+            self.points[3] = CGPoint(x: (self.points[2].x + self.points[4].x)/2.0,
+                                     y: (self.points[2].y + self.points[4].y)/2.0)
+            let path = UIBezierPath()
+            path.move(to: self.points[0])
+            path.addCurve(to: self.points[3], controlPoint1: self.points[1], controlPoint2: self.points[2])
+            if let _context = UIGraphicsGetCurrentContext() {
+                _context.addPath(path.cgPath)
+                _context.drawPath(using: .stroke)
+                _context.strokePath()
+                if let image = _context.makeImage() {
+                    self.drawingView.image = image
+                }
+            }
+            self.points[0] = self.points[3]
+            self.points[1] = self.points[4]
+            self.counter = 1
+        }
+        return
+        
+        
+        
+        
+        self.touchPoints.append(inPoint)
+        self.counter += 1
+        if self.counter < 3 {
+            return
+        }
+        self.counter = 0
+        let len:CGFloat = self.getDistance()
+        let factor:Float = Float(0.1/len)
+        let _xs = self.touchPoints.getArray()
+        let _ys = self.touchPoints.getArray(isX: false)
+        for i in stride(from: 0.1, to: 1.0, by: Double.Stride(factor)){
+            let _x = self.makeBazier1(t: CGFloat(i), points: _xs)
+            let _y = self.makeBazier1(t: CGFloat(i), points: _ys)
+            if let _context = UIGraphicsGetCurrentContext() {
+                _context.addEllipse(in: CGRect.init(origin: CGPoint(x: _x, y: _y) * self.drawingView.contentScaleFactor,
+                                                    size: CGSize(width: dotSize, height: dotSize)))
+    //            _context.move(to: CGPoint(x: 0, y: <#T##CGFloat#>))
+                _context.drawPath(using: .fill)
+                _context.fillPath()
+    //            _context.fillPath(using: CGPathFillRule.winding)
+                if let image = _context.makeImage() {
+                    self.drawingView.image = image
+                }
             }
         }
+        let _value  = self.touchPoints[self.touchPoints.count - 1]
+        self.touchPoints[self.touchPoints.count - 1] = self.touchPoints[self.touchPoints.count - 3]
+        self.touchPoints[self.touchPoints.count - 3] = _value
+        
+    }
+    
+    func makeBezier(t:CGFloat, points:[CGFloat])->CGFloat {
+      let t2 = t * t
+      let t3 = t2 * t
+      let mt = 1-t
+      let mt2 = mt * mt
+      let mt3 = mt2 * mt
+      return points[0]*mt3 + 3*points[1]*mt2*t + 3*points[2]*mt*t2 + points[3]*t3
+    }
+    
+    func makeBazier1(t:CGFloat, points:[CGFloat]) ->CGFloat {
+        let t2 = t * t
+        let mt = 1-t
+        let mt2 = mt * mt
+        return points[0]*mt2 + points[1]*2*mt*t + points[2]*t2
+    }
+    
+    func getDistance() -> CGFloat {
+    
+        let _len = self.touchPoints.count
+        var _x = self.touchPoints[_len - 3].x - self.touchPoints[_len - 1].x
+        _x *= _x
+        var _y = self.touchPoints[_len - 3].y - self.touchPoints[_len - 1].y
+        _y *= _y
+        return sqrt(_x + _y)
+        
     }
 }
 
 func *(_ a:CGPoint, _ b:CGFloat)->CGPoint{
-    return CGPoint(x: a.x*b - dotSize/2, y: (UIScreen.main.bounds.height - a.y)*b - dotSize/2)
+    return CGPoint(x: a.x * b - dotSize / 2, y: (UIScreen.main.bounds.height - a.y) * b - dotSize / 2)
+}
+
+extension Array where Iterator.Element == CGPoint {
+    
+    func getArray(isX:Bool = true) -> [CGFloat]{
+        var _data = [CGFloat]()
+        if isX {
+            for i in stride(from: self.count - 3, to: self.count, by: 1) {
+                _data.append(self[i].x)
+            }
+        } else {
+            for i in stride(from: self.count - 3, to: self.count , by: 1) {
+                _data.append(self[i].y)
+            }
+        }
+        return _data
+    }
 }
 
